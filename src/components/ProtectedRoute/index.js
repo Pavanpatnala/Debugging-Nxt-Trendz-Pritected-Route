c81980902e18d75293beb41c5a56964fd67f2b74
pavan <pavankumarpatnala96@gmail.com>
2023-03-04 06:57:03 +0000

    commit message

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,15 @@
+import {Route, Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+const ProtectedRoute = props => {
+  const token = Cookies.get('jwt_token')
+
+  if (token === undefined) {
+    return <Redirect to="/login" />
+  }
+
+  // FIX1: The spread operator should be used while sending the props
+  return <Route {...props} />
+}
+
+export default ProtectedRoute
